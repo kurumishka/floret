24f59ba1524b95216194a1d0b0d624495da1a416
emptiness <50295222+ketsushiri@users.noreply.github.com>
2022-05-16 12:46:24 +0300

    `gofmt`: spaces => tabs

--- api.go
+++ api.go
@@ -1,145 +1,145 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "math/rand"
-    "mime/multipart"
-    "net/http"
-    "net/url"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"mime/multipart"
+	"net/http"
+	"net/url"
 )
 
 const (
-    API_LINK = "https://api.vk.com/method/"
-    VERSION  = "5.131"
+	API_LINK = "https://api.vk.com/method/"
+	VERSION  = "5.131"
 )
 
 type ErrorT struct {
-    ErrorCode int    `json:"error_code"`
-    ErrorMsg  string `json:"error_msg"`
+	ErrorCode int    `json:"error_code"`
+	ErrorMsg  string `json:"error_msg"`
 }
 
 type Error struct {
-    ErrorV ErrorT `json:"error"`
+	ErrorV ErrorT `json:"error"`
 }
 
 func (e Error) String() string {
-    return fmt.Sprintf("%d: %s",
-        e.ErrorV.ErrorCode, e.ErrorV.ErrorMsg)
+	return fmt.Sprintf("%d: %s",
+		e.ErrorV.ErrorCode, e.ErrorV.ErrorMsg)
 }
 
 type GetUploadServerResponse struct {
-    UploadUrl string `json:"upload_url"`
-    AlbumId   int    `json:"album_id"`
-    UserId    int    `json:"user_id"`
+	UploadUrl string `json:"upload_url"`
+	AlbumId   int    `json:"album_id"`
+	UserId    int    `json:"user_id"`
 }
 
 type GetUploadServer struct {
-    GetUploadServerResponse `json:"response"`
+	GetUploadServerResponse `json:"response"`
 }
 
 type UploadServerResponse struct {
-    Server     int
-    PhotosList string `json:"photos_list"`
-    Aid        int
-    Hash       string
+	Server     int
+	PhotosList string `json:"photos_list"`
+	Aid        int
+	Hash       string
 }
 
 func callMethod(method string, params map[string]string) ([]byte, error) {
-    query := url.Values{}
-    for key, value := range params {
-        query.Add(key, value)
-    }
-    link := API_LINK + method + "?" + query.Encode()
-    logging(true, "GET:", link)
-    resp, err := http.Get(link)
-    if err != nil {
-        return make([]byte, 0), err
-    }
-    defer resp.Body.Close()
-    cont, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return make([]byte, 0), err
-    }
-    errInstance := new(Error)
-    json.Unmarshal(cont, errInstance)
-    if errInstance.ErrorV.ErrorCode != 0 {
-        return make([]byte, 0), fmt.Errorf("%s", errInstance.String())
-    }
-    return cont, nil
+	query := url.Values{}
+	for key, value := range params {
+		query.Add(key, value)
+	}
+	link := API_LINK + method + "?" + query.Encode()
+	logging(true, "GET:", link)
+	resp, err := http.Get(link)
+	if err != nil {
+		return make([]byte, 0), err
+	}
+	defer resp.Body.Close()
+	cont, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return make([]byte, 0), err
+	}
+	errInstance := new(Error)
+	json.Unmarshal(cont, errInstance)
+	if errInstance.ErrorV.ErrorCode != 0 {
+		return make([]byte, 0), fmt.Errorf("%s", errInstance.String())
+	}
+	return cont, nil
 }
 
 func validateConfig(config *Config) bool {
-    params := map[string]string{
-        "access_token": config.Token,
-        "owner_id":     fmt.Sprintf("-%s", config.Gid),
-        "album_id":     config.Aid,
-        "count":        "1",
-        "v":            VERSION,
-    }
-    _, err := callMethod("photos.get", params)
-    if err != nil {
-        logging(true, err)
-        return false
-    }
-    return true
+	params := map[string]string{
+		"access_token": config.Token,
+		"owner_id":     fmt.Sprintf("-%s", config.Gid),
+		"album_id":     config.Aid,
+		"count":        "1",
+		"v":            VERSION,
+	}
+	_, err := callMethod("photos.get", params)
+	if err != nil {
+		logging(true, err)
+		return false
+	}
+	return true
 }
 
 func getUploadServer(config *Config) (string, error) {
-    params := map[string]string{
-        "access_token": config.Token,
-        "group_id":     config.Gid,
-        "album_id":     config.Aid,
-        "v":            VERSION,
-    }
-    cont, err := callMethod("photos.getUploadServer", params)
-    if err != nil {
-        return "", err
-    }
-    upserver := new(GetUploadServer)
-    json.Unmarshal(cont, upserver)
-    if upserver.UploadUrl != "" {
-        return upserver.UploadUrl, nil
-    } else {
-        return "", fmt.Errorf("getUploadServer: ошибка демаршализации.")
-    }
+	params := map[string]string{
+		"access_token": config.Token,
+		"group_id":     config.Gid,
+		"album_id":     config.Aid,
+		"v":            VERSION,
+	}
+	cont, err := callMethod("photos.getUploadServer", params)
+	if err != nil {
+		return "", err
+	}
+	upserver := new(GetUploadServer)
+	json.Unmarshal(cont, upserver)
+	if upserver.UploadUrl != "" {
+		return upserver.UploadUrl, nil
+	} else {
+		return "", fmt.Errorf("getUploadServer: ошибка демаршализации.")
+	}
 }
 
 func uploadOnServer(config *Config, upserver string) (*UploadServerResponse, error) {
-    body := new(bytes.Buffer)
-    writer := multipart.NewWriter(body)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
 
-    logging(true, "создаём multipart форму...")
-    for i := 0; i < 5; i++ {
-        file := config.Pictures[rand.Intn(len(config.Pictures))]
-        fname := fmt.Sprintf("file%d", i)
-        part, err := writer.CreateFormFile(fname, file.Name)
-        if err != nil {
-            writer.Close()
-            return nil, err
-        }
-        part.Write(file.Content)
-    }
-    writer.Close()
-    logging(true, "POST:", upserver)
+	logging(true, "создаём multipart форму...")
+	for i := 0; i < 5; i++ {
+		file := config.Pictures[rand.Intn(len(config.Pictures))]
+		fname := fmt.Sprintf("file%d", i)
+		part, err := writer.CreateFormFile(fname, file.Name)
+		if err != nil {
+			writer.Close()
+			return nil, err
+		}
+		part.Write(file.Content)
+	}
+	writer.Close()
+	logging(true, "POST:", upserver)
 
-    resp, err := http.Post(upserver, writer.FormDataContentType(), body)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Post(upserver, writer.FormDataContentType(), body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    cont, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return nil, err
-    }
-    response := new(UploadServerResponse)
-    json.Unmarshal(cont, response)
+	cont, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	response := new(UploadServerResponse)
+	json.Unmarshal(cont, response)
 
-    if response.PhotosList == "" {
-        return nil, fmt.Errorf("сервер вернул неожиданный ответ: %s", cont)
-    }
-    return response, nil
+	if response.PhotosList == "" {
+		return nil, fmt.Errorf("сервер вернул неожиданный ответ: %s", cont)
+	}
+	return response, nil
 }
--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "math/rand"
-    "os"
-    "strings"
-    "time"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
-    BUFSIZE = 2048
-    ART     = `
+	BUFSIZE = 2048
+	ART     = `
       ________                __ 
      / ____/ /___  ________  / /_
     / /_  / / __ \/ ___/ _ \/ __/
@@ -24,224 +24,224 @@ const (
 )
 
 var (
-    verbose  = flag.Bool("v", false, "подробные логи.")
-    colorize = flag.Bool("c", false, "расскрашивать картинки. (не реализовано)")
-    noCaps   = flag.Bool("no-caps", false, "не прикреплять описания.")
+	verbose  = flag.Bool("v", false, "подробные логи.")
+	colorize = flag.Bool("c", false, "расскрашивать картинки. (не реализовано)")
+	noCaps   = flag.Bool("no-caps", false, "не прикреплять описания.")
 
-    token = flag.String("token", "", "использовать заданный токен.")
-    aid   = flag.String("aid", "", "использовать заданный album_id.")
-    gid   = flag.String("gid", "", "использовать заданный group_id.")
+	token = flag.String("token", "", "использовать заданный токен.")
+	aid   = flag.String("aid", "", "использовать заданный album_id.")
+	gid   = flag.String("gid", "", "использовать заданный group_id.")
 
-    configDir = flag.String("conf", "./res/config.json", "файл конфига.")
-    picsDir   = flag.String("pics", "./res/pics/", "директория с картинками.")
-    capsDir   = flag.String("caps", "./res/captions.conf", "директория с описаниями.")
+	configDir = flag.String("conf", "./res/config.json", "файл конфига.")
+	picsDir   = flag.String("pics", "./res/pics/", "директория с картинками.")
+	capsDir   = flag.String("caps", "./res/captions.conf", "директория с описаниями.")
 
-    threads = flag.Uint64("th", 1, "кол-во потоков.")
-    iters   = flag.Uint64("i", 1, "кол-во итерации.")
-    timeout = flag.Uint64("t", 0, "время ожидания (сек) между итерациями.")
+	threads = flag.Uint64("th", 1, "кол-во потоков.")
+	iters   = flag.Uint64("i", 1, "кол-во итерации.")
+	timeout = flag.Uint64("t", 0, "время ожидания (сек) между итерациями.")
 )
 
 var logging = func(extra bool, args ...interface{}) {
-    if extra && !*verbose {
-        return
-    }
-    log.Println(args...)
+	if extra && !*verbose {
+		return
+	}
+	log.Println(args...)
 }
 
 type Picture struct {
-    Content []byte
-    Name    string
+	Content []byte
+	Name    string
 }
 
 type Attachments struct {
-    Pictures []Picture
-    Captions []string
+	Pictures []Picture
+	Captions []string
 }
 
 type Credits struct {
-    Token string
-    Gid   string `json:"group_id"`
-    Aid   string `json:"album_id"`
+	Token string
+	Gid   string `json:"group_id"`
+	Aid   string `json:"album_id"`
 }
 
 type Config struct {
-    Credits
-    *Attachments
+	Credits
+	*Attachments
 }
 
 func (c Credits) String() string {
-    return fmt.Sprintf("[%d_%d]", c.Gid, c.Aid)
+	return fmt.Sprintf("[%d_%d]", c.Gid, c.Aid)
 }
 
 func (c Config) String() string {
-    return fmt.Sprintf("Token = %q; Gid = %q; Aid = %q; Pics = %d; Caps = %d.",
-        c.Token, c.Gid, c.Aid, len(c.Pictures), len(c.Captions))
+	return fmt.Sprintf("Token = %q; Gid = %q; Aid = %q; Pics = %d; Caps = %d.",
+		c.Token, c.Gid, c.Aid, len(c.Pictures), len(c.Captions))
 }
 
 func parseConfigCredits(path string) (*Credits, error) {
-    cont, err := ioutil.ReadFile(path)
-    if err != nil {
-        return nil, err
-    }
-    credits := new(Credits)
-    json.Unmarshal(cont, credits)
-    return credits, nil
+	cont, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	credits := new(Credits)
+	json.Unmarshal(cont, credits)
+	return credits, nil
 }
 
 func parseConfigAttachments(picsPath, capsPath string) (*Attachments, error) {
-    pics, err := os.ReadDir(picsPath)
-    if err != nil {
-        return nil, err
-    }
-    attachments := new(Attachments)
-    if !*noCaps {
-        caps, err := ioutil.ReadFile(capsPath)
-        if err != nil {
-            return nil, err
-        }
-        attachments.Captions = strings.Split(string(caps), "\n\n")
-    }
-    if !strings.HasSuffix(picsPath, "/") {
-        picsPath += "/"
-    }
-    logging(false, "инициализируем картинки...")
-    var (
-        failed    = 0
-        predicate = func(name string) bool {
-            return strings.HasSuffix(name, ".jpg") ||
-                strings.HasSuffix(name, ".png") ||
-                strings.HasSuffix(name, ".jpeg")
-        }
-    )
-    for _, pic := range pics {
-        if predicate(pic.Name()) {
-            fname := picsPath + pic.Name()
-            cont, err := ioutil.ReadFile(fname)
-            if err != nil {
-                logging(true, err)
-                logging(true, "не удалось инициализировать", pic.Name())
-                failed++
-                continue
-            }
-            attachments.Pictures = append(attachments.Pictures, Picture{cont, fname})
-        }
-    }
-    logging(false, fmt.Sprintf("%d/%d картинок инициализировано.",
-        len(attachments.Pictures),
-        len(attachments.Pictures)+failed))
-    return attachments, nil
+	pics, err := os.ReadDir(picsPath)
+	if err != nil {
+		return nil, err
+	}
+	attachments := new(Attachments)
+	if !*noCaps {
+		caps, err := ioutil.ReadFile(capsPath)
+		if err != nil {
+			return nil, err
+		}
+		attachments.Captions = strings.Split(string(caps), "\n\n")
+	}
+	if !strings.HasSuffix(picsPath, "/") {
+		picsPath += "/"
+	}
+	logging(false, "инициализируем картинки...")
+	var (
+		failed    = 0
+		predicate = func(name string) bool {
+			return strings.HasSuffix(name, ".jpg") ||
+				strings.HasSuffix(name, ".png") ||
+				strings.HasSuffix(name, ".jpeg")
+		}
+	)
+	for _, pic := range pics {
+		if predicate(pic.Name()) {
+			fname := picsPath + pic.Name()
+			cont, err := ioutil.ReadFile(fname)
+			if err != nil {
+				logging(true, err)
+				logging(true, "не удалось инициализировать", pic.Name())
+				failed++
+				continue
+			}
+			attachments.Pictures = append(attachments.Pictures, Picture{cont, fname})
+		}
+	}
+	logging(false, fmt.Sprintf("%d/%d картинок инициализировано.",
+		len(attachments.Pictures),
+		len(attachments.Pictures)+failed))
+	return attachments, nil
 }
 
 func parseConfig() *Config {
-    config := Config{Credits: Credits{*token, *gid, *aid}}
-    logging(true, "читаем файл конфига...")
-    credits, err := parseConfigCredits(*configDir)
-    if err != nil {
-        logging(true, err)
-        logging(false, "ошибка, не удалось прочесть файл конфига.")
-        logging(false, "будут насильно использованы значения флагов -t, -gid, -aid.")
-    } else {
-        logging(true, "OK, конфиг успешно инициализирован.")
-        config.Credits = *credits
-    }
-    logging(true, "получаем список картинок и описаний...")
-    if config.Attachments, err = parseConfigAttachments(*picsDir, *capsDir); err != nil {
-        logging(true, err)
-        logging(false, "фатальная ошибка, не удалось получить необходимые данные.")
-        os.Exit(2)
-    }
-    if len(config.Attachments.Pictures) == 0 {
-        logging(false, "фатальная ошибка, не удалось найти ни одной картинки для загрузки.")
-        os.Exit(2)
-    }
-    setConfigFlags(&config)
-    logging(false, "OK, необходимые данные получены.")
-    logging(true, config)
-    return &config
+	config := Config{Credits: Credits{*token, *gid, *aid}}
+	logging(true, "читаем файл конфига...")
+	credits, err := parseConfigCredits(*configDir)
+	if err != nil {
+		logging(true, err)
+		logging(false, "ошибка, не удалось прочесть файл конфига.")
+		logging(false, "будут насильно использованы значения флагов -t, -gid, -aid.")
+	} else {
+		logging(true, "OK, конфиг успешно инициализирован.")
+		config.Credits = *credits
+	}
+	logging(true, "получаем список картинок и описаний...")
+	if config.Attachments, err = parseConfigAttachments(*picsDir, *capsDir); err != nil {
+		logging(true, err)
+		logging(false, "фатальная ошибка, не удалось получить необходимые данные.")
+		os.Exit(2)
+	}
+	if len(config.Attachments.Pictures) == 0 {
+		logging(false, "фатальная ошибка, не удалось найти ни одной картинки для загрузки.")
+		os.Exit(2)
+	}
+	setConfigFlags(&config)
+	logging(false, "OK, необходимые данные получены.")
+	logging(true, config)
+	return &config
 }
 
 func setConfigFlags(config *Config) {
-    check := func(field *string, value *string) {
-        if *value != "" {
-            *field = *value
-        }
-    }
-    check(&config.Token, token)
-    check(&config.Gid, gid)
-    check(&config.Aid, aid)
+	check := func(field *string, value *string) {
+		if *value != "" {
+			*field = *value
+		}
+	}
+	check(&config.Token, token)
+	check(&config.Gid, gid)
+	check(&config.Aid, aid)
 }
 
 func upload(config *Config, upserver string, ch chan<- string) {
-    start := time.Now()
-    serverAnswer, err := uploadOnServer(config, upserver)
-    if err != nil {
-        ch <- fmt.Sprintf("%.2fs ERR, upload_on_server, %v",
-            time.Since(start).Seconds(), err)
-        return
-    }
-    params := map[string]string{
-        "access_token": config.Token,
-        "group_id":     config.Gid,
-        "album_id":     config.Aid,
-        "server":       fmt.Sprintf("%d", serverAnswer.Server),
-        "photos_list":  serverAnswer.PhotosList,
-        "hash":         serverAnswer.Hash,
-        "v":            VERSION,
-    }
-    if !*noCaps {
-        params["caption"] = config.Captions[rand.Intn(len(config.Captions))]
-    }
-    _, err = callMethod("photos.save", params)
-    if err != nil {
-        ch <- fmt.Sprintf("%.2fs ERR, photos.save, %v",
-            time.Since(start).Seconds(), err)
-    } else {
-        ch <- fmt.Sprintf("%.2fs OK, картинки успешно сохранены.",
-            time.Since(start).Seconds())
-    }
+	start := time.Now()
+	serverAnswer, err := uploadOnServer(config, upserver)
+	if err != nil {
+		ch <- fmt.Sprintf("%.2fs ERR, upload_on_server, %v",
+			time.Since(start).Seconds(), err)
+		return
+	}
+	params := map[string]string{
+		"access_token": config.Token,
+		"group_id":     config.Gid,
+		"album_id":     config.Aid,
+		"server":       fmt.Sprintf("%d", serverAnswer.Server),
+		"photos_list":  serverAnswer.PhotosList,
+		"hash":         serverAnswer.Hash,
+		"v":            VERSION,
+	}
+	if !*noCaps {
+		params["caption"] = config.Captions[rand.Intn(len(config.Captions))]
+	}
+	_, err = callMethod("photos.save", params)
+	if err != nil {
+		ch <- fmt.Sprintf("%.2fs ERR, photos.save, %v",
+			time.Since(start).Seconds(), err)
+	} else {
+		ch <- fmt.Sprintf("%.2fs OK, картинки успешно сохранены.",
+			time.Since(start).Seconds())
+	}
 }
 
 func main() {
-    flag.Parse()
-    fmt.Println(ART)
-    config := parseConfig()
-    logging(false, "проверяем конфиг...")
-    ok := validateConfig(config)
-    if !ok {
-        logging(false, "фатальная ошибка, проверка конфига не удалась.")
-        os.Exit(2)
-    }
-    logging(false, "OK, конфиг прошёл проверку.")
-    logging(true, "получаем URL для загрузки картинок на сервер...")
-    upserver, err := getUploadServer(config)
-    if err != nil {
-        logging(false, err)
-        logging(false, "фатальная ошибка, не удалось получить URL для загрузки.")
-        os.Exit(2)
-    }
-    logging(true, "OK, UPSERVER:", upserver)
+	flag.Parse()
+	fmt.Println(ART)
+	config := parseConfig()
+	logging(false, "проверяем конфиг...")
+	ok := validateConfig(config)
+	if !ok {
+		logging(false, "фатальная ошибка, проверка конфига не удалась.")
+		os.Exit(2)
+	}
+	logging(false, "OK, конфиг прошёл проверку.")
+	logging(true, "получаем URL для загрузки картинок на сервер...")
+	upserver, err := getUploadServer(config)
+	if err != nil {
+		logging(false, err)
+		logging(false, "фатальная ошибка, не удалось получить URL для загрузки.")
+		os.Exit(2)
+	}
+	logging(true, "OK, UPSERVER:", upserver)
 
-    answer, err := uploadOnServer(config, upserver)
-    if err != nil {
-        logging(false, "не удалось загрузить картинки на сервер.")
-        logging(false, err)
-    }
-    logging(true, answer)
-    logging(false, "сохраняем картинки в альбом...")
-    logging(true, fmt.Sprintf("потоков = %d, итераций = %d.", *threads, *iters))
+	answer, err := uploadOnServer(config, upserver)
+	if err != nil {
+		logging(false, "не удалось загрузить картинки на сервер.")
+		logging(false, err)
+	}
+	logging(true, answer)
+	logging(false, "сохраняем картинки в альбом...")
+	logging(true, fmt.Sprintf("потоков = %d, итераций = %d.", *threads, *iters))
 
-    ch := make(chan string, BUFSIZE)
-    for i := uint64(1); i <= *iters; i++ {
-        logging(false, fmt.Sprintf("итерация номер %d...", i))
-        for j := uint64(0); j < *threads; j++ {
-            go upload(config, upserver, ch)
-        }
-        logging(false, "потоки запущены.")
-        for j := uint64(0); j < *threads; j++ {
-            logging(false, <-ch)
-        }
-        logging(true, fmt.Sprintf("TIMEOUT: %d секунд.", *timeout))
-        time.Sleep(time.Second * time.Duration(*timeout))
-    }
-    logging(false, "OK, успешно завершено.")
+	ch := make(chan string, BUFSIZE)
+	for i := uint64(1); i <= *iters; i++ {
+		logging(false, fmt.Sprintf("итерация номер %d...", i))
+		for j := uint64(0); j < *threads; j++ {
+			go upload(config, upserver, ch)
+		}
+		logging(false, "потоки запущены.")
+		for j := uint64(0); j < *threads; j++ {
+			logging(false, <-ch)
+		}
+		logging(true, fmt.Sprintf("TIMEOUT: %d секунд.", *timeout))
+		time.Sleep(time.Second * time.Duration(*timeout))
+	}
+	logging(false, "OK, успешно завершено.")
 }
